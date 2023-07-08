e803413d08289ce4957811723cbee59556e7b754
Shubham Mishra <shubhammishra10101998@gmail.com>
2023-07-08 17:46:19 +0530

    Add expense component completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,48 +4,64 @@ import { useLoaderData } from "react-router-dom";
 // components
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
 
 // helper functions
-import { createBudget, fetchData, waait } from "../helper";
+import { createBudget, createExpense, fetchData, waait } from "../helper";
 import { toast } from "react-toastify";
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  const budgets = fetchData('budgets');
+  const budgets = fetchData("budgets");
   return { userName, budgets };
 }
 
 // action
 export async function dashboardAction({ request }) {
-    await waait();
+  await waait();
 
   const data = await request.formData();
-  const {_action, ...values} = Object.fromEntries(data)
+  const { _action, ...values } = Object.fromEntries(data);
 
-//   new user submission
-if (_action === "newUser") {
+  //   new user submission
+  if (_action === "newUser") {
     try {
-        localStorage.setItem("userName", JSON.stringify(values.userName));
-        return toast.success(`Welcome, ${values.userName}`);
-      } catch (err) {
-        throw new Error("There was a problem creating your account.");
-      }
-}
-
-if (_action === 'createBudget') {
-    try {
-        // create budget
-        createBudget({
-            name: values.newBudget,
-            amount: values.newBudgetAmount
-        })
-
-        return toast.success('Budget created!')
+      localStorage.setItem("userName", JSON.stringify(values.userName));
+      return toast.success(`Welcome, ${values.userName}`);
     } catch (err) {
-        throw new Error('There was a problem creating your budget!')
+      throw new Error("There was a problem creating your account.");
     }
-}
+  }
+
+  if (_action === "createBudget") {
+    try {
+      // create budget
+      createBudget({
+        name: values.newBudget,
+        amount: values.newBudgetAmount,
+      });
+
+      return toast.success("Budget created!");
+    } catch (err) {
+      throw new Error("There was a problem creating your budget!");
+    }
+  }
+
+  if (_action === "createExpense") {
+    try {
+      // create expense
+      createExpense({
+        name: values.newExpense,
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget
+      })
+
+      return toast.success(`Expense ${values.newExpense} created!`);
+    } catch (err) {
+      throw new Error("There was a problem creating your expense!");
+    }
+  }
 }
 
 const Dashboard = () => {
@@ -59,12 +75,20 @@ const Dashboard = () => {
             Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            {/* {budgets ? () : ()} */}
-            <div className="grid-lg">
+            {budgets && budgets.length > 0 ? (
+              <div className="grid-lg">
                 <div className="flex-lg">
-                    <AddBudgetForm /> 
+                  <AddBudgetForm />
+                  <AddExpenseForm budgets={budgets} />
                 </div>
-            </div>
+              </div>
+            ) : (
+              <div className="grid-sm">
+                <p>Personal budgeting is the secret to financial freedom.</p>
+                <p>Create a budget to get started!</p>
+                <AddBudgetForm />
+              </div>
+            )}
           </div>
         </div>
       ) : (
